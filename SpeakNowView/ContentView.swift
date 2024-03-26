291da03a110139e366ffb11a7996541a5e0e151f
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-03-26 19:02:53 +1100

    Initial Commit

---
+++ SpeakNowView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeakNowView
+//
+//  Created by Sidak Singh Aulakh on 26/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeakNowView/SpeakNowViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakNowViewApp.swift
+//  SpeakNowView
+//
+//  Created by Sidak Singh Aulakh on 26/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpeakNowViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
