62369b7e26be2e92ba1c24680b1e271600859d64
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-04-01 20:15:29 +1100

    Class 26/03

--- SpeakNowView/ContentView.swift
+++ SpeakNowView/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var date: Date = Date()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(.speakNow)
+                .ignoresSafeArea()
+            VStack {
+                Text("AND I GO BACK TO")
+                    .foregroundStyle(.regularMaterial)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.black)
+                DatePicker(DateFormatter.month.string(from: date).uppercased(), selection: $date, displayedComponents: .date)
+                    .foregroundStyle(.regularMaterial)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.black)
+                Text("ALL THE TIME")
+                    .foregroundStyle(.regularMaterial)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.black)
+                
+            }
+            .padding()
         }
-        .padding()
+    }
+}
+
+extension DateFormatter {
+    static var month: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "d MMMM"
+        return formatter
     }
 }
 
